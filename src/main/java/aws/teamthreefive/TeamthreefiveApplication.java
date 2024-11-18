94ab18f5d83e0305ee2c378f9bcce149d66b07ab
meli <melitina915@gmail.com>
2024-11-18 22:29:41 +0900

    Initial Commit

---
+++ src/main/java/aws/teamthreefive/TeamthreefiveApplication.java
@@ -0,0 +1,13 @@
+package aws.teamthreefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamthreefiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamthreefiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aws/teamthreefive/TeamthreefiveApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.teamthreefive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamthreefiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
